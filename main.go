8181e1a3afdb4bccb6dacc7483895cf68a7aa4af
Victor Broman <victor.bo.broman@gmail.com>
2024-03-04 18:09:13 +0100

    cleans up

--- main.go
+++ main.go
@@ -8,11 +8,8 @@ import (
 )
 
 func main() {
-	mainContainer := mainContainer.MainContainer()
-	http.Handle("/", templ.Handler(mainContainer))
-	http.HandleFunc("/stuff", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello there")
-	})
+	http.Handle("/", templ.Handler(mainContainer.MainContainer()))
+
 	fmt.Println("Listening on 3000")
 	http.ListenAndServe(":3000", nil)
 }
